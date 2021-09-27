3514831f241bab44e85cde492db0ab234fc6390e
RicardoWebProject <ricardo.fuentes.o@outlook.com>
2021-09-26 21:28:14 -0600

    Modelo para Persona

---
+++ Backend/models/persona.js
@@ -0,0 +1,13 @@
+import mongoose, {Schema} from 'mongoose';
+
+const personaSchema = new Schema({
+    tipo_persona: {type: String, maxlength:20, required:true},
+    nombre: {type: String, maxlength:50, required:true},
+    num_documento: {type: Number, unique:true},
+    telefono: {type: String, maxlength: 9},
+    email: {type: String, maxlength:50, uninque:true},
+    estado: {type: Number, default:1},
+    createdAt: {type: Date, default: Date.now}
+});
+
+const Persona = mongoose.model('persona', personaSchema);

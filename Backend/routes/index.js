9bd7416a5e580312b82ee1a4a603b1698b07e2e0
RicardoWebProject <ricardo.fuentes.o@outlook.com>
2021-09-27 12:45:48 -0600

    Exportado el router de Usuario

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,8 +1,10 @@
 import routerx from 'express-promise-router';
 import personaRouter from './persona';
+import usuarioRouter from './usuario';
 
 const router = routerx();
 
 router.use('/persona', personaRouter);
+router.use('/usuario', usuarioRouter);
 
 export default router;

d476fbf8370ff4a1acda6764bc96e0d6361bbb6a
RicardoWebProject <ricardo.fuentes.o@outlook.com>
2021-09-27 19:42:27 -0600

    Modificado: De req , body -> req.body, en función add

--- Backend/controllers/personaController.js
+++ Backend/controllers/personaController.js
@@ -4,7 +4,7 @@ export default{
     //Agregar Persona
     add: async(req, res, next) => {
         try {
-            const reg = await models.Persona.create(req, body);
+            const reg = await models.Persona.create(req.body);
             res.status(200).json(reg);
         } catch (error) {
             res.status(500).send({
